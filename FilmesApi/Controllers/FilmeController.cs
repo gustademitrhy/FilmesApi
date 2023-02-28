ae49bbe57bfc3f53876f20482ad0f9a77a45ec39
GUSTAVO DEMITRHY CORDEIRO TELES <124848@pmenos.com.br>
2023-02-28 13:01:13 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,45 @@
+﻿
+using FilmesApi.Data;
+using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
+    {
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+       return  CreatedAtAction(nameof(RecuperaFilmePorId), new {id = filme.Id},
+            filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuparFilmes([FromQuery] int skip = 0,
+        [FromQuery] int take = 50)
+    {
+        return _context.Filmes.Skip(skip).Take(take);
+    }
+
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
+        return Ok(filme);
+    }
+
+}
---
+++ FilmesApi/Data/FilmeContext.cs
@@ -0,0 +1,16 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace FilmesApi.Data;
+
+public class FilmeContext : DbContext
+{
+	public FilmeContext(DbContextOptions<FilmeContext> opts)
+		: base(opts)
+	{
+
+	}
+	
+	public DbSet<Filme> Filmes { get; set; }
+}
